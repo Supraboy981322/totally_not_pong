0e8965367d724dce6201f6c5c340166c9e57b79d
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-04-21 13:09:44 -0500

    player movement

--- rewrite/src/main.zig
+++ rewrite/src/main.zig
@@ -5,16 +5,39 @@ pub fn main() !void {
     rl.initWindow(800, 450, "totally not pong (100%)");
     defer rl.closeWindow();
 
+    // WARNING: DO NOT REMOVE THIS (for now)
+    // on my particular Hyprland install (NixOS + Nvidia + scrolling)
+    //  window launches on wrong monitor and Hyprland thinks it's on the
+    //    primary monitor, so if you don't move the window before attempting
+    //      to change windows, Hyprland crashes entirely
+    if (rl.getMonitorCount() > 1)
+        rl.setWindowMonitor(1);
+    
     rl.setTargetFPS(60);
 
-    var p1:Player = .init(10, 20);
+    var p1:Player = .init(15, 50);
 
     while (!rl.windowShouldClose()) {
         rl.beginDrawing();
         defer rl.endDrawing();
 
+        const screen_height, _ = .{
+            rl.getScreenHeight(),
+            rl.getScreenWidth(),
+        };
+
+        p1.shape.height = @floatFromInt(@divTrunc(screen_height, 10));
+        p1.shape.width = @floatFromInt(@divTrunc(@as(u32, @intFromFloat(p1.shape.height)), 5));
+
         rl.clearBackground(.black);
         rl.drawRectangleRec(p1.shape, p1.color);
-        p1.shape.y += 1;
+
+       p1.speed = @floatFromInt(@divTrunc(screen_height, 30));
+
+        if (rl.isKeyDown(.k) and p1.can_move(.up))
+            p1.shape.y -= p1.speed;
+
+        if (rl.isKeyDown(.j) and p1.can_move(.down))
+            p1.shape.y += p1.speed;
     }
 }
--- rewrite/src/player.zig
+++ rewrite/src/player.zig
@@ -3,10 +3,12 @@ const rl = @import("raylib");
 pub const Player = struct {
     color:rl.Color,
     shape:rl.Rectangle,
+    speed:f32,
 
     pub fn init(width:i32, height:i32) Player {
         return .{
             .color = .white,
+            .speed = 2,
             .shape = .{
                 .x = 10,
                 .y = @floatFromInt(@divExact((rl.getScreenWidth() - height), @as(u32, 2))),
@@ -14,5 +16,25 @@ pub const Player = struct {
                 .height = @floatFromInt(height),
             },
         };
+    }
+
+    pub fn can_move(self:*Player, direction:enum{ up, down }) bool {
+        const pos =
+            if (direction == .down)
+                self.shape.y + self.shape.height
+            else
+                self.shape.y;
+
+        const limit:f32 =
+            if (direction == .down)
+                @floatFromInt(rl.getScreenHeight())
+            else
+                0;
+
+        return
+            if (direction == .down)
+                pos < limit
+            else
+                pos > limit;
     }
 };
