7f8784ee6772407df306871dbc7d1771b3a4b2b0
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-04-21 13:10:23 -0500

    cleanup flake

--- raylib/go/flake.nix
+++ raylib/go/flake.nix
@@ -3,24 +3,26 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, flake-utils }:
     (flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in devShells.default = pkgs.mkShell ({
-        packages = with pkgs; [
-          go
+      in {
+        devShells.default = pkgs.mkShell ({
+          packages = with pkgs; [
+            go
 
-          # Raylib-Go reps
-          mesa
-          libXi
-          libXcursor
-          libXrandr
-          libglvnd
-          libXinerama
-          wayland
-          libxkbcommon
-        ];
+            # raylib-go deps
+            mesa
+            libXi
+            libXcursor
+            libXrandr
+            libglvnd
+            libXinerama
+            wayland
+            libxkbcommon
+          ];
+        });
       })
-    ))
+    );
 }
