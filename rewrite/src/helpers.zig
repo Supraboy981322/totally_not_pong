b2dc21d9a137bbf798e92e3a1363f5e782c4a8f3
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-04-21 19:22:05 -0500

    helper for input boxes ; helper to quickly convert string to number disregarding validity

--- rewrite/src/helpers.zig
+++ rewrite/src/helpers.zig
@@ -1,5 +1,86 @@
+const std = @import("std");
 const rl = @import("raylib");
+const types = @import("types.zig");
 
 pub fn is_ctrl_down() bool {
     return rl.isKeyDown(.left_control) or rl.isKeyDown(.right_control);
+}
+
+pub fn fast_n_to_s(T:type, n:T, alloc:std.mem.Allocator) ![:0]const u8 {
+    var len:usize = 0;
+    var num:T = n;
+    while (num > 0) : (len += 1){
+        num = @divFloor(num, 10);
+    }
+    const buf = try alloc.alloc(u8, len+1);
+    defer alloc.free(buf);
+    const end = std.fmt.printInt(buf, n, 10, .lower, .{});
+    return try alloc.dupeZ(u8, buf[0..end]);
+}
+
+pub fn input_box(state:*types.State, alloc:std.mem.Allocator, y_pos:f32) !void {
+    var input_txt = &state.aux_arraylist;
+
+    const txt_box:rl.Rectangle = b: {
+        const screen_width = @as(f32, @floatFromInt(rl.getScreenWidth()));
+        const width:f32 = screen_width * 0.9;
+        break :b .{
+            .x = screen_width * 0.05,
+            .y = y_pos,
+            .width = width,
+            .height = 35,
+        };
+    };
+
+    {
+        var itr = try @import("keys.zig").KeyItr.init(alloc);
+        defer itr.deinit(alloc);
+        while (itr.next()) |key| switch (key.tag) {
+            .backspace => _ = input_txt.pop(),
+            else => {
+                const key_int:c_int = @intFromEnum(key.tag);
+                if (key_int >= 32 and key_int <= 125) {
+                    try input_txt.append(alloc, @intCast(key_int));
+                }
+            },
+        };
+    }
+
+    rl.drawRectangleRec(txt_box, .white);
+
+    const visible_input_buf:[:0]const u8 = b: {
+        var itr = std.mem.reverseIterator(input_txt.items);
+        var buf = try std.ArrayList(u8).initCapacity(alloc, 0);
+        defer buf.deinit(alloc);
+        var buf_elderly = try alloc.allocSentinel(u8, 0, 0);
+        while (itr.next()) |b| {
+            try buf.append(alloc, b);
+            alloc.free(buf_elderly);
+            buf_elderly = try alloc.dupeZ(u8, buf.items);
+            const buf_len = @as(f32, @floatFromInt(rl.measureText(buf_elderly, 20)));
+            if (buf_len > txt_box.width) break;
+        }
+        for (0..3) |_| _ = buf.pop();
+        alloc.free(buf_elderly);
+        buf_elderly = try alloc.dupeZ(u8, buf.items);
+        std.mem.reverse(u8, buf_elderly);
+        break :b buf_elderly;
+    };
+    defer alloc.free(visible_input_buf);
+
+    rl.drawText(
+        visible_input_buf,
+        @intFromFloat(txt_box.x + 5),
+        @intFromFloat(txt_box.y + 8),
+        20,
+        .dark_gray,
+    );
+
+    if ((state.frame_count / 20) % 2 == 0) rl.drawText(
+        "_",
+        @as(i32, @intFromFloat(txt_box.x)) + 8 + rl.measureText(visible_input_buf, 20),
+        @intFromFloat(txt_box.y + 12),
+        20,
+        .dark_gray
+    );
 }
